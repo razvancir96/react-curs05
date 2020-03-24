c017de2694b4f3ebef1fa6048c6936162fe349c4
razvancir96 <>
2020-03-24 17:12:41 +0200

    console.log props: history, location, match

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,10 @@ import React from 'react';
 import Layout from '../components/Layout';
 
 function Home(props) {
+    // Pentru ca Home este inclusa intr-o componenta de tip Route, va contine 3 prop-uri in plus:
+    // match, history, location. Uitati-va in consola pentru a vedea ce contine fiecare.
+    console.log(props);
+
     return(
         <div>
             {/* Tot ce este pus intre <Layout> si </Layout> va reprezenta props.children in cadrul componentei Layout.*/}
